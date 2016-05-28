1e7ff2e605a5a382b4676d29e6bd683dbd3ef9da
Hendrik Werner <hendrik.to@gmail.com>
2016-05-28 19:05:12 +0200

    Add a stub for a runnable using taxis.

---
+++ assignment14_1/TaxiRunner.java
@@ -0,0 +1,20 @@
+package assignment14_1;
+
+/**
+ *
+ * @author Hendrik Werner // s4549775
+ */
+public class TaxiRunner implements Runnable {
+
+    private final Taxi taxi;
+
+    public TaxiRunner(Taxi t) {
+        taxi = t;
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
