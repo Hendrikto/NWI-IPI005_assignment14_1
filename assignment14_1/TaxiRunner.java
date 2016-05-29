14312994ef1eb64ae4e50a2dd2d35651fb84741c
Hendrik Werner <hendrik.to@gmail.com>
2016-05-29 15:51:31 +0200

    Add JavaDoc.

--- assignment14_1/TaxiRunner.java
+++ assignment14_1/TaxiRunner.java
@@ -1,13 +1,18 @@
 package assignment14_1;
 
 /**
+ * A runnable wrapper of taxis to allow for concurrency.
  *
  * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
  */
 public class TaxiRunner implements Runnable {
 
     private final Taxi taxi;
 
+    /**
+     * @param t the taxi
+     */
     public TaxiRunner(Taxi t) {
         taxi = t;
     }
