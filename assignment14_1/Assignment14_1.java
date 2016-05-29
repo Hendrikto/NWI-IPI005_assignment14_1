c1573e821177c0f97facd1d1f1d2f96e4d123532
Hendrik Werner <hendrik.to@gmail.com>
2016-05-29 15:34:04 +0200

    Add JavaDoc.

--- assignment14_1/Assignment14_1.java
+++ assignment14_1/Assignment14_1.java
@@ -1,11 +1,15 @@
 package assignment14_1;
 
 /**
- *
  * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
+ * @author Sjaak Smetsers
  */
 public class Assignment14_1 {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         Simulation sim = new Simulation();
         while (!sim.ended()) {
